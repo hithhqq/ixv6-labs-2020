1b0762c1771fc6ee4fe33a743d8be565d5bf3c43
yeye <2798758158@qq.com>
2024-04-22 20:23:34 +0800

    完成第二个lab

--- kernel/proc.c
+++ kernel/proc.c
@@ -106,7 +106,7 @@ allocproc(void)
 
 found:
   p->pid = allocpid();
-
+  p->mask = 0;
   // Allocate a trapframe page.
   if((p->trapframe = (struct trapframe *)kalloc()) == 0){
     release(&p->lock);
@@ -149,6 +149,7 @@ freeproc(struct proc *p)
   p->chan = 0;
   p->killed = 0;
   p->xstate = 0;
+  p->mask = 0;
   p->state = UNUSED;
 }
 
@@ -276,7 +277,7 @@ fork(void)
   np->sz = p->sz;
 
   np->parent = p;
-
+  np->mask = p->mask;
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
 
@@ -692,4 +693,21 @@ procdump(void)
     printf("%d %s %s", p->pid, state, p->name);
     printf("\n");
   }
+}
+
+
+uint64 free_proc(void)
+{
+  uint64 n = 0;
+  struct proc *p;
+  for (p = proc; p < &proc[NPROC]; ++p)
+  {
+    acquire(&p->lock);
+    if (p->state != UNUSED)
+    {
+      n++;
+    }
+    release(&p->lock);
+  }
+  return n;
 }
